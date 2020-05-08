3d0ee4881cb0f0cb265b67385ce6ab88affde8db
Angelo Ibarrola <vino_1105@hotmail.com>
2020-05-07 20:22:26 -0700

    added navbar and overall styling

--- app_folder/forms.py
+++ app_folder/forms.py
@@ -1,6 +1,6 @@
 from flask import flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, RadioField, FileField, SelectField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, RadioField, FileField, SelectField, TextAreaField, TextField
 from wtforms.validators import DataRequired
 
 from app_folder import app
@@ -26,7 +26,6 @@ class RegistrationForm(FlaskForm):
     username = StringField('Name', validators=[DataRequired()])
     famchoice = SelectField('Family: ',choices=[('Blue', 'Blue Ox'),('Red', 'Red Dragon'),('Yellow', 'Yellow Monkey'), ('Green', 'Green Snake')])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Register')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
@@ -36,7 +35,7 @@ class RegistrationForm(FlaskForm):
 
 
 class SendMessageForm(FlaskForm):
-    userMessage = StringField('What is your message:', validators=[DataRequired()])
+    userMessage = TextAreaField('What is your message:', validators=[DataRequired()])
     userFrom = StringField('Who is it by', validators=[DataRequired()])
     submit = SubmitField(' Send Message ')
     
