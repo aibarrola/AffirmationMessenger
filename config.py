cea4163a512ee6dc35343728e43836123e5cbda3
Angelo Ibarrola <vino_1105@hotmail.com>
2020-05-09 10:15:53 -0700

    modify config

--- config.py
+++ config.py
@@ -4,5 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config():
     SECRET_KEY = 'you-will-never-guess'
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
