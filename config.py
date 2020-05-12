9e242eb35be01f6a64a7e0fe305713b8c3262c7b
Angelo Ibarrola <vino_1105@hotmail.com>
2020-05-12 10:01:16 -0700

    added Click Above to homepage

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config():
     SECRET_KEY = 'you-will-never-guess'
 
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
